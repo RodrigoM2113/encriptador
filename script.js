d46bfa7d753d6e23b39b4ae6b5db96c720bf7ecc
Rodrigo Mosqueda <rdmosquedav@gmail.com>
2023-05-18 21:33:22 -0600

    subiendo proyecto encriptador

---
+++ script.js
@@ -0,0 +1,81 @@
+const textArea = document.querySelector(".entrada");
+const mensaje = document.querySelector(".salida");
+const btnc = document.getElementById('btnc')
+const derecha = document.querySelector(".salida")
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado
+    textArea.value= ""
+    derecha.style.backgroundImage = "none"
+    
+
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+        
+    }
+
+    return stringEncriptada
+    
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado
+    textArea.value= ""
+    derecha.style.backgroundImage = "none"
+
+
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+
+    }
+
+    return stringDesencriptada
+    
+}
+
+btnc.addEventListener('click', e =>{
+    mensaje.select();
+    document.execCommand('copy');
+
+
+})
+
+
+var text =function (e){
+
+    var event = e || window.event;
+    var key = event.keyCode || event.wchich;
+    var tecla = String.fromCharCode(key);
+
+    var patron = /^[a-z\s]*$/;
+
+    if (!tecla.search(patron)){
+
+        return true;
+
+    }else{
+
+        return false;
+                
+    }
+    
+}
+
